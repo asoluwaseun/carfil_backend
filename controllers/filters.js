fda271cca9a951f1a4d9d0d75963c184b55c66c6
asoluwaseun <soadesina71@student.lautech.edu.ng>
2020-04-01 13:25:58 +0100

    Completed fetch filters endpoint

--- controllers/filters.js
+++ controllers/filters.js
@@ -4,7 +4,14 @@ const Models = require('../database/models/')
 class Filters{
     static  async getFilters(req, res){
         try{
+            let filters = await Models.filters.findAll()
 
+            let response = {
+                data: filters,
+                message: filters.length > 0 ? `${filters.length} found` : "No filter found"
+            }
+
+            res.status(200).json(response)
         }
         catch(err){
             res.sendStatus(500)
